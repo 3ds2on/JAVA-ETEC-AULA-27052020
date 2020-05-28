d0f3e29c6fb8e08206d787d16c99f3612ad58d46
B1-08 <B1-08@B1-08-PC>
2020-05-27 21:54:58 -0300

    Commite do tio!

---
+++ Aula2705_Data/src/pct/TesteData.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pct;
+
+/**
+ *
+ * @author B1-08
+ */
+public class TesteData {
+    public static void main(String[] args) {
+        //Criação e instância de um objeto da classe data
+        Data data = new Data();
+        
+        
+    }
+}
